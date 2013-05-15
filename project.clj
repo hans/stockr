e7c48f11ae3347ba5e8f9abb4d49e1aee5d7b793
Hans Engel <engel@engel.uk.to>
2013-05-15 14:05:55 -0700

    Working ARFF generation (via CLI)

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.2"]
                  [com.leadtune/clj-ml "0.2.5"]
                  [com.novemberain/monger "1.6.0-beta2"]])
--- src/stockr/arff_generator.clj
+++ src/stockr/arff_generator.clj
@@ -1,14 +1,21 @@
 (ns stockr.arff-generator
-  (require [[monger.core :as m]
-            [clj-ml.io :as i]]))
+  (:require [monger.core :as m]
+            [monger.collection :as mc]
+            [clj-ml.data :as d]
+            [clojure.tools.cli :as c])
+  (:use [monger.conversion :only [from-db-object]]))
 
-(def *moving-average-width*
+(def quote-db-host "localhost")
+(def quote-db-name "stocks")
+(def quote-db-collection "quotes")
+
+(def moving-average-width
   "The width of the window used to build a moving average of stock
    quotes."
 
   5)
 
-(def *change-threshold*
+(def change-threshold
   "The minimum change in stock price required such that the change be
    considered a 'drop' or 'rise' rather than a 'hold.'"
 
@@ -35,52 +42,57 @@
   "Attributes used exclusively in the classification model"
 
   (concat attributes-fetch attributes-common
-          '(:NextBehavior)))
+          '({:NextBehavior [:drop :hold :rise]})))
 
 (defmulti get-attribute
   "Fetch an attribute for a given instance."
-  (fn [attr _ _] attr))
+  (fn [attr _ _]
+    (if (map? attr)
+      (first (keys attr))
+      attr)))
 
 (defmethod get-attribute :MovingAverage [attr instances i]
-  (let [lower-bound (max 0 (- i *moving-average-width*))
-        xs (take *moving-average-width* (drop lower-bound instances))]))
+  (let [lower-bound (max 0 (- i moving-average-width))
+        xs (take moving-average-width (drop lower-bound instances))
+        prices (map :LastPrice xs)]
+    (/ (reduce + prices) (count prices))))
 
 (defmethod get-attribute :VolumeChange [attr instances i]
   (let [instance (nth instances i)
         prev-instance (nth instances (dec i))]
-    (- ("Volume" instance) ("Volume" prev-instance))))
+    (- (:Volume instance) (:Volume prev-instance))))
 
 (defmethod get-attribute :NextHigh [attr instances i]
-  ("High" (nth instances (inc i))))
+  (:High (nth instances (inc i))))
 
 (defmethod get-attribute :NextLow [attr instances i]
-  ("Low" (nth instances (inc i))))
+  (:Low (nth instances (inc i))))
 
 (defmethod get-attribute :NextVolume [attr instances i]
-  ("Volume" (nth instances (inc i))))
+  (:Volume (nth instances (inc i))))
 
 (defmethod get-attribute :NextBehavior
+  [attr instances i]
   "Provide a class label for an example which specifies how the
    referenced stock behaves in the following instance.
 
    - A `drop` class indicates that the example which directly follows
      this one has a price which has dropped more than
-     `*change-threshold` relative to the current price.
+     `change-threshold` relative to the current price.
    - A `hold` class indicates that the share price for this example and
      the one which directly follows it do not change more than +/-
-     `*change-threshold*`.
+     `change-threshold`.
    - A `rise` class indicates that the example which directly follows
-     this one has a price which has risen more than `*change-threshold`
+     this one has a price which has risen more than `change-threshold`
      relative to the current price."
-  [attr instances i]
 
-  (let [price ((nth instances i) "LastPrice")
-        next-price ((nth instances (inc i)) "LastPrice")
+  (let [price (:LastPrice (nth instances i))
+        next-price (:LastPrice (nth instances (inc i)))
         price-change (- next-price price)]
     (cond
-     (> price-change *change-threshold*) :rise
-     (< price-change (- *change-threshold*)) :drop
-     :else :h)))
+     (> price-change change-threshold) :rise
+     (< price-change (- change-threshold)) :drop
+     :else :hold)))
 
 ;; All other attributes should come from the instance document
 (defmethod get-attribute :default [attr instances i]
@@ -96,4 +108,26 @@
   "Get all quotes for a given symbol."
   [symbol]
 
-  )
+  (m/connect! {:host quote-db-host})
+  (m/set-db! (m/get-db quote-db-name))
+  (mc/find-maps quote-db-collection {:Symbol symbol}))
+
+(def cli-spec
+  [["-s" "--symbol" "Stock for which to generate data"]
+   ["--classify" "Build data for a classifier" :flag true]
+   ["--regression" "Build data for a regression algorithm" :flag true]])
+
+(defn -main [& args]
+  (let [[options args banner] (apply (partial c/cli args) cli-spec)
+
+        symbol (:symbol options)
+        quotes (stock-quotes symbol)
+        ;; Ignore the first and last quotes, which don't have previous /
+        ;; next quotes (some generated features will break)
+        quote-indices (range 1 (dec (count quotes)))
+
+        attributes attributes-classification
+        data (map #(make-example % quotes attributes) quote-indices)
+        boom (print (first data))
+        relation-name (str "stock_quotes_" symbol)]
+    (print (d/make-dataset relation-name attributes data))))
