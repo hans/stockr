5168f241e8847eab53fe5112424c1bd12cbd8c70
Hans Engel <engel@engel.uk.to>
2013-05-15 19:16:38 -0700

    clj-ml 0.3.5

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [com.leadtune/clj-ml "0.2.5"]
                  [com.novemberain/monger "1.6.0-beta2"]])
+                 [cc.artifice/clj-ml "0.3.5"]
